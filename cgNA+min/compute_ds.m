a08dc61d6cbad3665993fadbfa2ad91508c4f578
singh-raushan <raushan@iitm.ac.in>
2025-01-06 19:31:55 +0530

    Delete cgNA+min directory

--- cgNA+min/compute_ds.m
+++
@@ -1,8 +0,0 @@
-function dirs = compute_ds(qvec)
-dirs = zeros(3,3);
-q1 = qvec(1); q2 = qvec(2); q3 = qvec(3); q4 = qvec(4);
-qnormsq = q1^2+q2^2+q3^2+q4^2;
-dirs(:,1) = (1/qnormsq)*[q1^2-q2^2-q3^2+q4^2;2*q1*q2+2*q3*q4;2*q1*q3-2*q2*q4];
-dirs(:,2) = (1/qnormsq)*[2*q1*q2-2*q3*q4;-q1^2+q2^2-q3^2+q4^2;2*q1*q4+2*q2*q3];
-dirs(:,3) = (1/qnormsq)*[2*q1*q3+2*q2*q4;-2*q1*q4+2*q2*q3;-q1^2-q2^2+q3^2+q4^2];
-return
